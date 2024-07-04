bc88a8ce952678226654c5491ea11f455555910e
shriharijadhav <mr.shrihari212@gmail.com>
2024-07-04 14:45:58 +0530

    pre-loader added-scale animation added

--- src/components/template/DoorAnimation.jsx
+++ src/components/template/DoorAnimation.jsx
@@ -12,7 +12,7 @@ const DoorAnimation = () => {
     >
       <Flex w="100%" h="70%">
         {/* Content inside the reveal animation */}
-        <Flex  rounded={'full'} outline={'7px solid white'} w="400px" h="400px" bg="purple.300" direction={'column'} gap={'20px'} justifyContent={'center'} alignItems={'center'} textAlign={'center'} p={'40px 40px'}>
+        <Flex className='scale-animation' rounded={'full'} outline={'7px solid white'} w="400px" h="400px" bg="purple.300" direction={'column'} gap={'20px'} justifyContent={'center'} alignItems={'center'} textAlign={'center'} p={'40px 40px'}>
           <Flex justifyContent={'center'} alignItems={'center'} direction={'column'} gap={'20px'} pt={'10px'}>
             <Text className='metal_Script' fontSize={'xx-large'}>Join us in celebrating our special day! </Text>
             <Text className='metal_Script' fontSize={'x-large'}>Your presence would make our day truly memorable.</Text>
