63b83b7089204890082988273478a3fed82f7ee4
shriharijadhav <mr.shrihari212@gmail.com>
2024-08-01 11:25:30 +0530

    portfolio code - done

--- src/components/template/SectionSeven.jsx
+++ src/components/template/SectionSeven.jsx
@@ -9,14 +9,14 @@ const SectionSeven = () => {
     <Flex className='metal_Script' color={'black'} p={'20px 0px'} justifyContent={'center'} alignItems={'center'} w={'100%'} bg={'linear-gradient(109.6deg, rgb(238, 164, 179) 11.2%, rgb(212, 153, 234) 47.3%, rgb(150, 121, 255) 100.2%)'}>
       <Flex w={'90%'} justifyContent={'center'} alignItems={'center'} direction={'column'}>
         <Text fontSize={'x-large'} p={'10px 0px'}>Inviter</Text>
-        <Text fontSize={'large'}>Pritam Awatade</Text>
+        <Text fontSize={'large'}>Shrihari Jadhav</Text>
         <Flex fontSize={'large'} justifyContent={'center'} alignItems={'center'} gap={'5px'}>
           <MdCall/>
-          <Text  as="a" href="tel:+919763657894">+919763657894</Text>
+          <Text  as="a" href="tel:+919970758021">+919970758021</Text>
         </Flex>
         <Flex fontSize={'large'} justifyContent={'center'} alignItems={'center'} gap={'5px'}>
           <MdEmail/>
-          <Text as="a" href="mailto:pritamawatade@gmail.com">pritamawatade@gmail.com</Text>
+          <Text as="a" href="mailto:mr.shrihari212@gmail.com">mr.shrihari212@gmail.com</Text>
         </Flex>
       </Flex>
       <AudioToggleMute/>        
